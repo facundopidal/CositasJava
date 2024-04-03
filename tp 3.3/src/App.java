82e7de1e40e8a491dc76e66187c9214daacd7cfb
shuliannnn <109643724+shuliannnn@users.noreply.github.com>
2024-04-03 19:53:30 -0300

    Add files via upload

---
+++ tp 3.3/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Cliente cliente1 = new Cliente("Juan", 1, 'm');
+        Cuenta cuenta1 = new Cuenta(1, 10000, cliente1);
+        cuenta1.extraer(3000);
+        cuenta1.extraer(7000);
+        cuenta1.extraer(3000);
+        cuenta1.depositar(10000);
+        cuenta1.extraer(2000);
+        cuenta1.depositar(9000);
+        cuenta1.extraer(3000);
+        cuenta1.depositar(8000);
+        cuenta1.extraer(4000);
+        cuenta1.depositar(7000);
+        cuenta1.extraer(5000);
+        cuenta1.depositar(6000);
+        cuenta1.extraer(6000);
+
+        System.out.println();
+        cuenta1.mostrarRegistros();
+    }
+}
---
+++ tp 3.3/src/Cliente.java
@@ -0,0 +1,42 @@
+public class Cliente {
+    private String nombre;
+    private int id;
+    private char genero;
+    
+    public Cliente(String nombre, int id, char genero) {
+        this.nombre = nombre;
+        this.id = id;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public void mostrar(){
+        System.out.println("ID: "+id);
+        System.out.println("Nombre: "+nombre);
+        System.out.println((genero == 'm') ? "Genero: Masculino" : "Genero Femenino");
+    }
+    
+}
